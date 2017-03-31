96568e0ed230f61db175d4b6badf526e60435ff9
Nick Benoit <nick.benoit14@gmail.com>
2017-03-31 17:04:59 -0600

    break out defaults

---
+++ defaults.js
@@ -0,0 +1,48 @@
+var defaultConfig = {
+  index: 'index.html',
+  region: 'us-east-1',
+  uploadDir: '.',
+  prefix: '',
+  corsConfiguration: [],
+  enableCloudfront: false,
+  retries: 20
+}
+
+var templateConfig = Object.assign({},
+  defaultConfig,
+  {
+    domain: 'sample.bucket.name',
+    corsConfiguration: [{
+      AllowedMethods: [ /* required */
+        'STRING_VALUE_REQUIRED'
+        /* more items */
+      ],
+      AllowedOrigins: [ /* required */
+        'STRING_VALUE_REQUIRED'
+        /* more items */
+      ],
+      AllowedHeaders: [
+        'STRING_VALUE'
+        /* more items */
+      ],
+      ExposeHeaders: [
+        'STRING_VALUE'
+        /* more items */
+      ],
+      MaxAgeSeconds: 0
+    }]
+  }
+)
+
+var defaultBucketConfig = {
+  Bucket: '' /* required */
+}
+
+var defaultWebsiteConfig = {
+  Bucket: '', /* required */
+  WebsiteConfiguration: { /* required */
+    IndexDocument: {
+      Suffix: defaultConfig.index /* required */
+    }
+  }
+}
--- index.js
+++ index.js
@@ -14,55 +14,6 @@ var s3diff = require('s3-diff')
 var logUpdate = require('log-update')
 var array = require('lodash/array')
 
-var defaultConfig = {
-  index: 'index.html',
-  region: 'us-east-1',
-  uploadDir: '.',
-  prefix: '',
-  corsConfiguration: [],
-  enableCloudfront: false,
-  retries: 20
-}
-
-var templateConfig = Object.assign({},
-  defaultConfig,
-  {
-    domain: 'sample.bucket.name',
-    corsConfiguration: [{
-      AllowedMethods: [ /* required */
-        'STRING_VALUE_REQUIRED'
-        /* more items */
-      ],
-      AllowedOrigins: [ /* required */
-        'STRING_VALUE_REQUIRED'
-        /* more items */
-      ],
-      AllowedHeaders: [
-        'STRING_VALUE'
-        /* more items */
-      ],
-      ExposeHeaders: [
-        'STRING_VALUE'
-        /* more items */
-      ],
-      MaxAgeSeconds: 0
-    }]
-  }
-)
-
-var defaultBucketConfig = {
-  Bucket: '' /* required */
-}
-
-var defaultWebsiteConfig = {
-  Bucket: '', /* required */
-  WebsiteConfiguration: { /* required */
-    IndexDocument: {
-      Suffix: defaultConfig.index /* required */
-    }
-  }
-}
-
 
 function mergeResults (oldResult, newResult) {
   var updated = oldResult.updated.concat(newResult.updated)
